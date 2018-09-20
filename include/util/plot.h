48ceaccaa3213c41c65aa80bf378cdbf21ad3dfc
Sanaxen <bccatfalcon@gmail.com>
2018-09-21 00:34:02 +0900

    plotting image save to file

--- include/util/plot.h
+++ include/util/plot.h
@@ -26,19 +26,25 @@ class gnuPlot
 	}
 
 public:
+	std::string save_image_name = "image.png";
 	std::string script_name = "plot.plt";
 	std::string data_name = "plot.dat";
 
+	bool save_image = false;
 	std::string title = "";
 	float linewidth = 2.0;
 	float pointsize = 1.0;
 
-	gnuPlot(std::string& gnuplot_exe_path_, const int script_id=-1)
+	std::string linecolor = "";
+
+	gnuPlot(std::string& gnuplot_exe_path_, const int script_id=-1, bool save_image_=false)
 	{
 		gnuplot_exe_path = gnuplot_exe_path_;
 
 		id = script_id;
 		if (script_id < 0) id = rand();
+
+		save_image = save_image_;
 
 		char buff[512];
 		sprintf(buff, "plot_%04d.plt", id);
@@ -47,11 +53,21 @@ public:
 		sprintf(buff, "plot_%04d.dat", id);
 		data_name = buff;
 
+		sprintf(buff, "image_%04d.png", id);
+		data_name = buff;
+
 		script = fopen(script_name.c_str(), "w");
 
 		
 		fprintf(script, "set border lc rgb \"black\"\n");
 		fprintf(script, "set grid lc rgb \"#D8D8D8\" lt 2\n");
+		//fprintf(script, "set colorsequence default\n");
+		fprintf(script, "set colorsequence podo\n");
+		
+		if (save_image)
+		{
+			fprintf(script, "set terminal png\n");
+		}
 	}
 	
 	
@@ -111,19 +127,38 @@ public:
 			every = "every " + std::to_string(every_num);
 		}
 
-		if (headers.size()) title = "t " + headers[0];
+		if (headers.size())
+		{
+			title = (char*)headers[0].c_str();
+			if (title[0] != '\"')
+			{
+				title = (std::string("t \"") + headers[0] + std::string("\"")).c_str();
+			}
+		}
 
+		if (linecolor != "")
+		{
+			linecolor = "lc " + linecolor;
+		}
 		const char* plot = (plot_count) ? "replot" : "plot";
-		fprintf(script,"%s '%s' %s using %d %s with lines linewidth %.1f\n",
-			plot, data_name.c_str(), every.c_str(), 1, title.c_str(), linewidth);
+		fprintf(script,"%s '%s' %s using %d %s with lines linewidth %.1f %s\n",
+			plot, data_name.c_str(), every.c_str(), 1, title.c_str(), linewidth, linecolor.c_str());
 
 		for ( int i = 1; i < X.n; i++ )
 		{
 			title = "";
-			if (headers.size() > i ) title = "t " + headers[i];
-			fprintf(script,"replot '%s' %s using %d %s with lines linewidth %.1f\n",
-				data_name.c_str(), every.c_str(), i+1, title.c_str(), linewidth);
+			if (headers.size() > i)
+			{
+				title = (char*)headers[i].c_str();
+				if (title[0] != '\"')
+				{
+					title = (std::string("t \"") + headers[i] + std::string("\"")).c_str();
+				}
 		}
+			fprintf(script,"replot '%s' %s using %d %s with lines linewidth %.1f %s\n",
+				data_name.c_str(), every.c_str(), i+1, title.c_str(), linewidth, linecolor.c_str());
+		}
+		linecolor = "";
 		plot_count++;
 	}
 
@@ -142,35 +177,59 @@ public:
 			int every_num = X.m / maxpoint;
 			every = "every " + std::to_string(every_num);
 		}
-		if (headers.size()) title = (char*)headers[0].c_str();
+		if (headers.size())
+		{
+			title = (char*)headers[0].c_str();
+			if (title[0] != '\"')
+			{
+				title = (std::string("t \"") + headers[0] + std::string("\"")).c_str();
+			}
+		}
+
+		if (linecolor != "")
+		{
+			linecolor = "lc " + linecolor;
+		}
 
 		const char* plot = (plot_count) ? "replot" : "plot";
-		fprintf(script, "%s '%s' %s using %d t %s with points pointsize %.1f\n",
-			plot, data_name.c_str(), every.c_str(), 1, title, pointsize);
+		fprintf(script, "%s '%s' %s using %d t %s with points pointsize %.1f %s\n",
+			plot, data_name.c_str(), every.c_str(), 1, title, pointsize, linecolor.c_str());
 
 		for (int i = 1; i < X.n; i++)
 		{
 			title = "";
-			if (headers.size() > i) title = (char*)headers[i].c_str();
-			fprintf(script, "replot '%s' %s using %d t %s with points pointsize %.1f\n",
-				data_name.c_str(), every.c_str(), i+1, title, pointsize);
+			if (headers.size() > i)
+			{
+				title = (char*)headers[i].c_str();
+				if (title[0] != '\"')
+				{
+					title = (std::string("t \"") + headers[i] + std::string("\"")).c_str();
 		}
+			}
+			fprintf(script, "replot '%s' %s using %d t %s with points pointsize %.1f %s\n",
+				data_name.c_str(), every.c_str(), i+1, title, pointsize, linecolor.c_str());
+		}
+		linecolor = "";
 		plot_count++;
 	}
 
-	void scatter(Matrix<dnn_double>&X, Matrix<dnn_double>&Y, int pointtype=6, char* palette ="rgbformulae 34,35,36", int maxpoint = -1)
+	void scatter(Matrix<dnn_double>&X, Matrix<dnn_double>&Y, char* label_text, int pointtype=6, char* palette ="rgbformulae 34,35,36", int maxpoint = -1)
 	{
 		script_reopen();
 		if (script == NULL) return;
 		fprintf(script, "set datafile separator \",\"\n");
 
 		Matrix<dnn_double> x = X.Col(0);
+		Matrix<dnn_double>& y = Y.Col(0);
+
 		x = x.appendCol(Y.Col(0));
 		if (palette)
 		{
 			x = x.appendCol(Y.Col(0));
 			set_palette(palette);
 		}
+		y = y / (y.Max() - y.Min());
+		x = x.appendCol(y);
 		x.print_csv((char*)data_name.c_str());
 
 		std::string every = "";
@@ -181,21 +240,28 @@ public:
 			every = "every " + std::to_string(every_num);
 		}
 
+		std::string label = "";
+		if (label_text)
+		{
+			label = "t \"" + std::string(label_text) + "\"";
+		}
+
 		const char* plot = (plot_count) ? "replot" : "plot";
 		if (palette)
 		{
-			fprintf(script, "%s '%s' %s using 1:2:3 with points pointsize %.1f pt %d lc palette\n",
-				plot, data_name.c_str(), every.c_str(), pointsize, pointtype);
+			fprintf(script, "%s '%s' %s using 1:2:3 %s with points pointsize %.1f pt %d lc palette\n",
+				plot, data_name.c_str(), every.c_str(), label.c_str(), pointsize, pointtype);
 		}
 		else
 		{
-			fprintf(script, "%s '%s' %s using 1:2 with points pointsize %.1f pt %d\n",
-				plot, data_name.c_str(), every.c_str(), pointsize, pointtype);
+			fprintf(script, "%s '%s' %s using 1:2 %s with points pointsize %.1f pt %d\n",
+				plot, data_name.c_str(), every.c_str(), label.c_str(), pointsize, pointtype);
 		}
+		linecolor = "";
 		plot_count++;
 	}
 
-	void plot_histogram(Matrix<dnn_double>&X)
+	void plot_histogram(Matrix<dnn_double>&X, char* label_text =NULL)
 	{
 		script_reopen();
 		if (script == NULL) return;
@@ -204,10 +270,20 @@ public:
 
 		fprintf(script, "set style fill solid border  lc rgb \"black\"\n");
 
+		if (linecolor != "")
+		{
+			linecolor = "lc " + linecolor;
+		}
+		std::string label = "";
+		if (label_text)
+		{
+			label = "t \"" + std::string(label_text) + "\"";
+		}
 		const char* plot = (plot_count) ? "replot" : "plot";
-		fprintf(script, "%s '%s' using 1:2 with boxes linewidth %.1f\n",
-			plot, data_name.c_str(), linewidth);
+		fprintf(script, "%s '%s' using 1:2 %s with boxes linewidth %.1f %s\n",
+			plot, data_name.c_str(), label.c_str(), linewidth, linecolor.c_str());
 
+		linecolor = "";
 		plot_count++;
 	}
 
@@ -227,7 +303,15 @@ public:
 	void draw()
 	{
 		if (script == NULL) return;
-		fprintf(script, "pause 10\n");
+		if (save_image)
+		{
+			fprintf(script, "set out \"%s\"\n", save_image_name.c_str());
+			fprintf(script, "replot\n");
+		}
+		else
+		{
+		fprintf(script, "pause 3\n");
+		}
 		close();
 		system((gnuplot_exe_path + " " + script_name).c_str());
 	}
