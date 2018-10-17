7be31eefd9b7166a058908d67525a1663e417972
Sanaxen <bccatfalcon@gmail.com>
2018-10-17 20:24:41 +0900

    add sample

--- example/sample_test/nonliner/test1/run2.bat
+++ example/sample_test/nonliner/test1/run2.bat
@@ -8,7 +8,7 @@ set progress=1
 if "%1"=="0" set progress=0
 
 del /Q images\*.png
-%LDM%\NonLinearRegression.exe  --x 1 --y 1 --csv sample2.csv --test 0 --progress %progress%  --plot 30 --epochs 120
+%LDM%\NonLinearRegression.exe  --x 1 --y 1 --csv sample2.csv --test 0 --progress %progress%  --plot 10 --epochs 200
 :--minibatch_size 30 --learning_rate 0.1 --n_layers 4 --input_unit 64
 
 ffmpeg -y -i "images/test_%%04d.png" -r 2 -crf 30 ccc.mp4
