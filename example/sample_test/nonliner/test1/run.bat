86698941a341f3c7bd17ead797d52f7710871a9f
Sanaxen <bccatfalcon@gmail.com>
2018-12-05 23:07:04 +0900

    update

--- example/sample_test/nonliner/test1/run.bat
+++ example/sample_test/nonliner/test1/run.bat
@@ -12,7 +12,7 @@ del /Q images\*.png
 :%LDM%\NonLinearRegression.exe  --header 1 --x 2 --y 3 --x_var "x1" --x_var "x2" --csv sample3.csv --test 0 --progress %progress%  --plot 5
 :%LDM%\NonLinearRegression.exe  --header 1 --x 2 --y 3  --y_var "y1" --y_var "y2" --y_var "y3" --csv sample3.csv --test 0 --progress %progress%  --plot 5
 
-%LDM%\NonLinearRegression.exe  --x 2 --y 3 --csv sample.csv --test 0.0 --tol 0.01 --progress %progress%  --plot 5 --epochs 6000  --minibatch_size 64
+%LDM%\NonLinearRegression.exe  --x 2 --y 3 --csv sample.csv --test 0.5 --tol 0.01 --progress %progress%  --plot 5 --epochs 6000  --minibatch_size 64
 goto end
 
 %LDM%\NonLinearRegression.exe  --x 2 --y 3 --csv sample.csv --test 0.0 --tol 0.05 --progress %progress%  --plot 5 --epochs 6000
@@ -21,6 +21,7 @@ ffmpeg -y -i "images/test_%%04d.png" -r 2 -crf 30 aaa.mp4
 ffmpeg -y -i "images/test_%%04d.png" -r 2 -crf 30 aaa.flv
 ffmpeg -y -i "images/test_%%04d.png" -an -r 10  -pix_fmt rgb24 -vf "setpts=2.0*PTS"  -vf "scale=500:-1" -f gif aaa.gif
 
+:end
+
 del *.dll
 
-:end
--- example/sample_test/nonliner/test2/run.bat
+++ example/sample_test/nonliner/test2/run.bat
@@ -10,7 +10,7 @@ if "%1"=="0" set progress=0
 del /Q images\*.png
 :%LDM%\TimeSeriesRegression.exe --header 1  --x 2 --y 3 --x_var "t1" --x_var "t2" --y_var "y1" --y_var "y2" --y_var "y3" --csv sample2.csv --test 0.5 --progress %progress% --plot 50 --tol 0.014
 
-%LDM%\TimeSeriesRegression.exe  --x 2 --y 3 --csv sample.csv --test 0.5 --progress %progress% --plot 5 --tol 0.05 --epochs 600 --n_layers 5 --seq_len 64 --input_unit 32 --minibatch_size 64
+%LDM%\TimeSeriesRegression.exe  --x 2 --y 3 --csv sample.csv --test 0.5 --progress %progress% --plot 5 --tol 0.01 --epochs 600 --n_layers 5 --seq_len 64 --input_unit 32 --minibatch_size 64 --ref_prev 15
 
 goto end
 :%LDM%\TimeSeriesRegression.exe  --x 2 --y 3 --csv sample.csv --test 0.5 --progress %progress% --plot 5 --tol 0.01 --epochs 600 --n_layers 8 --seq_len 20 --input_unit 64
@@ -25,6 +25,7 @@ ffmpeg -y -i "images/test_%%04d.png" -r 2 -crf 30 bbb.flv
 ffmpeg -y -i "images/test_%%04d.png" -an -r 10  -pix_fmt rgb24 -vf "setpts=2.0*PTS"  -vf "scale=500:-1" -f gif bbb.gif
 
 
+:end
+
 del *.dll
 
-:end
