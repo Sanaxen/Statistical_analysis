7b20c1ee37403bf4299eccd15f8fd0efcaeb7756
Sanaxen <bccatfalcon@gmail.com>
2019-05-26 20:11:16 +0900

    Update run_train.bat

--- example/sample_test/nonliner/test9/run_train.bat
+++ example/sample_test/nonliner/test9/run_train.bat
@@ -9,10 +9,10 @@ if "%1"=="0" set progress=0
 
 del /Q images\*.png
 
-%LDM%\TimeSeriesRegression.exe  --x 0 --y 1 --y_var D1 --t_var 0 --csv qtdbsel102_train.csv --learning_rate 0.1 --opt_type adam ^
---test 0.3 --progress %progress% --plot 5 --tol 0.001  --header 1 ^
---epochs 26000 --n_layers 20  --n_rnn_layers 1  --normal zscore  --rnn_type gru --use_cnn 0 ^
---seq_len 20 --minibatch_size 20 --hidden_size 200 --early_stopping 1 --test_mode 0 ^
+%LDM%\TimeSeriesRegression.exe  --x 0 --y 1 --y_var D1 --t_var 0 --csv qtdbsel102_train.csv --learning_rate 0.5 --opt_type adam ^
+--test 0.2 --progress %progress% --plot 5 --tol 0.001  --header 1 ^
+--epochs 26000 --n_layers 10  --n_rnn_layers 1  --normal zscore  --rnn_type lstm --use_cnn 0 ^
+--seq_len 30 --minibatch_size 30 --hidden_size 20 --early_stopping 1 --test_mode 0 ^
 --out_seq_len 3
 
 goto end
